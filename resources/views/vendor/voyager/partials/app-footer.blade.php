d9723e87f55159cee5d7d5e0991d63085af09250
franck7514 <60940389+franck7514@users.noreply.github.com>
2020-06-08 11:05:59 -0500

    Add files via upload

---
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -0,0 +1,14 @@
+{{--
+/**
+ * @ Author: @CristianMarinT
+ * @ Create Time: 2020-08-01 10:59:22
+ * @ Modified by: @CristianMarinT
+ * @ Modified time: 2020-08-01 12:11:45
+ * @ Description:
+ */
+--}}
+<footer class="app-footer">
+        <div class="site-footer-right">
+                Hecho por {!! setting('admin.creator') !!}
+        </div>
+</footer> 
